900c2d05e0660ed2aa1e1cc9fbf7c4509586b56e
Minhao Zhang <zhangminhao12138@gmail.com>
2023-10-26 14:56:20 +0200

    trying to fix the problem with marker icon missing on github pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "leaflet/dist/leaflet.css";
 import Router from "./Components/Router";
 
 const root = ReactDOM.createRoot(
